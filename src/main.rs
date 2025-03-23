a1bb659108e1942c6c00c525b6ab4a4180277cd2
Matěj Laitl <matej@laitl.cz>
2025-03-23 15:47:11 +0100

    Split to components

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,23 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Stylesheet { href: CSS }
+        Title {}
+        DogView {}
+    }
+}
+
+#[component]
+fn Title() -> Element {
+    rsx! {
         div { id: "title",
             h1 { "HotDog! 🌭" }
         }
+    }
+}
+
+#[component]
+fn DogView() -> Element {
+    rsx! {
         div { id: "dogview",
             img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
         }
