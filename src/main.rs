43d1173ad99fdd17f10baa3d6af69ce396619a74
Matěj Laitl <matej@laitl.cz>
2025-03-23 15:26:43 +0100

    Follow template: assets, CSS

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 use dioxus::prelude::*;
 
+static CSS: Asset = asset!("/assets/main.css");
+
 fn main() {
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
-    rsx! { "HotDog!" }
+    rsx! {
+        document::Stylesheet { href: CSS }
+        div { id: "title",
+            h1 { "HotDog! 🌭" }
+        }
+        div { id: "dogview",
+            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+        }
+        div { id: "buttons",
+            button { id: "skip", "skip" }
+            button { id: "save", "save!" }
+        }
+    }
 }
