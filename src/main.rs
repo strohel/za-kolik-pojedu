e6a399fcf9de0cf09a016673fe2a6bdf3b1c043c
Matěj Laitl <matej@laitl.cz>
2025-05-12 21:36:36 +0200

    Try to get current time in browser - doesn't work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::provider::{car4way::Car4way, Provider, ProviderKind};
 use dioxus::prelude::*;
+use time::{Duration, OffsetDateTime, PrimitiveDateTime, UtcDateTime, UtcOffset};
 use tracing::debug;
 
 pub mod provider;
@@ -33,6 +34,8 @@ fn Title() -> Element {
 #[component]
 fn MainView() -> Element {
     debug!("MainView rendering...");
+    let input_data = use_signal(|| TripInputData::new().expect("can construct TripInputData"));
+
     let providers = use_signal(|| -> Vec<Provider> {
         vec![
             Provider::new(ProviderKind::Bolt(Default::default())),
@@ -41,7 +44,7 @@ fn MainView() -> Element {
     });
 
     rsx! {
-        TripInput {},
+        TripInput { input_data },
         div { id: "providers", class: "top-section",
             h2 { "Poskytovatelé" },
             for provider in providers.read().iter().cloned() {
@@ -51,15 +54,39 @@ fn MainView() -> Element {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct TripInputData {
+    km: f64,
+    begin: PrimitiveDateTime,
+    end: PrimitiveDateTime,
+}
+
+impl TripInputData {
+    fn new() -> Result<Self, RenderError> {
+        const FIVE_MINUTES: i64 = 5 * 60;
+
+        let now = UtcDateTime::now().unix_timestamp();
+
+        // Round to next five minutes
+        let in_five_mins = ((now / FIVE_MINUTES) + 1) * FIVE_MINUTES;
+        // Convert to OffsetDateTime in local TZ
+        let in_five_mins = OffsetDateTime::from_unix_timestamp(in_five_mins)?
+            .to_offset(UtcOffset::current_local_offset()?);
+        // Convert to PrimitiveDateTime
+        let in_five_mins = PrimitiveDateTime::new(in_five_mins.date(), in_five_mins.time());
+
+        Ok(Self { km: 10.0, begin: in_five_mins, end: in_five_mins + Duration::HOUR })
+    }
+}
+
 #[component]
-fn TripInput() -> Element {
+fn TripInput(input_data: Signal<TripInputData>) -> Element {
     debug!("TripInput rendering...");
-    let mut input_kilometers = use_signal(|| 0);
 
     let kilometers_changed = move |evt: Event<FormData>| {
         debug!("Kilometers changed: {evt:?}");
-        let value: i32 = evt.value().parse()?;
-        input_kilometers.set(value);
+        let value: f64 = evt.value().parse()?;
+        input_data.write().km = value;
 
         Ok(())
     };
@@ -71,7 +98,7 @@ fn TripInput() -> Element {
                 label { for: "input-kilometers", "Počet km " },
                 input { id: "input-kilometers",
                     r#type: "number",
-                    value: input_kilometers,
+                    value: input_data().km,
                     onchange: kilometers_changed,
                     min: 0,
                 },
